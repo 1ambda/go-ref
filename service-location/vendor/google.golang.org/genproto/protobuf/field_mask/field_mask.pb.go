ab6fdefc1dfcd42c624a72c3e148868bc656f129
1ambda <1amb4a@gmail.com>
2018-04-25 19:45:29 +0900

    chore(location): vendor update

--- service-location/vendor/google.golang.org/genproto/protobuf/field_mask/field_mask.pb.go
+++ service-location/vendor/google.golang.org/genproto/protobuf/field_mask/field_mask.pb.go
@@ -231,8 +231,8 @@ const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 //
 // ## Field Mask Verification
 //
-// The implementation of the all the API methods, which have any FieldMask type
-// field in the request, should verify the included field paths, and return
+// The implementation of any API method which has a FieldMask type field in the
+// request should verify the included field paths, and return an
 // `INVALID_ARGUMENT` error if any path is duplicated or unmappable.
 type FieldMask struct {
 	// The set of field mask paths.
